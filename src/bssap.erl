6a2eded46e9eb36a0fc131d4f813491c2fefaf6c
Astrid Smith <astrid@xrtc.net>
2012-07-04 02:55:45 -0700

    BSSAP sublayer dispatcher

---
+++ src/bssap.erl
@@ -0,0 +1,25 @@
+-module(bssap).
+-author("Duncan Smith <Duncan@xrtc.net>").
+
+% State machines for BSSAP sublayer dispatch (picking between BSSMAP
+% and DTAP)
+
+% sccp discriminator types
+-define(SCCP_DISCRIM_DTAP,	1).
+-define(SCCP_DISCRIM_BSSMAP,	0).
+
+-export([parse_message/1]).
+
+% BSSMAP message
+parse_message(<<?SCCP_DISCRIM_BSSMAP:8, Length:8, Message:Length/binary>>) ->
+    {ok, bssmap, bssmap_codec:parse_bssmap(Message)};
+
+% DTAP message
+parse_message(<<?SCCP_DISCRIM_DTAP:8, DLCI:8, Length:8, Message:Length/binary>>) ->
+    {ok, dtap, dtap_codec:parse_message(DLCI, Message)};
+
+% Something else entirely
+parse_message(<<Discrim:8, Bin/binary>>) ->
+    {ok, unknown, Discrim, Bin}.
+
+
